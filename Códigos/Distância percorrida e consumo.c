3863529c1bb8ea787c73e005a2c1764c24453d20
jessic-campos <jessicacampos.acude@gmail.com>
2022-12-12 20:16:54 -0300

    Atualização

--- Códigos/Distância percorrida e consumo.c
+++ Códigos/Distância percorrida e consumo.c
@@ -6,12 +6,12 @@ int main (){
     
     scanf("%f %f", &t, &v);
     
-    d = v * t;
+    d = v * t; 
     l = d / 12;
     
     printf("A distância percorrida foi de %.2fKM/h com o consumo de %.2fL.", d,l);
     
     return 0;
-    
+
     
 }
